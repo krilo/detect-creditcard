30157bb75e4ebcc79cf3e585522c03d6ed4796fa
Kristofer Forsell <kristofer@Kristofers-MacBook-Pro-2.local>
2015-04-15 21:51:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# detect-creditcard
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+Detects credit card type from number
+
+## Usage
+
+[![NPM](https://nodei.co/npm/detect-creditcard.png)](https://www.npmjs.com/package/detect-creditcard)
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/krilo/detect-creditcard/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,44 @@
+
+function detectCreditcard(number){
+  var re = {
+    electron: /^(4026|417500|4405|4508|4844|4913|4917)\d+$/,
+    maestro: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\d+$/,
+    dankort: /^(5019)\d+$/,
+    interpayment: /^(636)\d+$/,
+    unionpay: /^(62|88)\d+$/,
+    visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+    mastercard: /^5[1-5][0-9]{14}$/,
+    amex: /^3[47][0-9]{13}$/,
+    diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+    discover: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
+    jcb: /^(?:2131|1800|35\d{3})\d{11}$/
+  };
+
+  if (re.electron.test(number)) {
+      return 'ELECTRON';
+  } else if (re.maestro.test(number)) {
+      return 'MAESTRO';
+  } else if (re.dankort.test(number)) {
+      return 'DANKORT';
+  } else if (re.interpayment.test(number)) {
+      return 'INTERPAYMENT';
+  } else if (re.unionpay.test(number)) {
+      return 'UNIONPAY';
+  } else if (re.visa.test(number)) {
+      return 'VISA';
+  } else if (re.mastercard.test(number)) {
+      return 'MASTERCARD';
+  } else if (re.amex.test(number)) {
+      return 'AMEX';
+  } else if (re.diners.test(number)) {
+      return 'DINERS';
+  } else if (re.discover.test(number)) {
+      return 'DISCOVER';
+  } else if (re.jcb.test(number)) {
+      return 'JCB';
+  } else {
+      return undefined;
+  }
+}
+
+module.exports = detectCreditcard;
